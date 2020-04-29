7485390b3c8774cd55daa81a393f28bbb944d142
MXCQ <761405246@qq.com>
2020-04-29 11:11:28 +0800

    Updates

---
+++ precache-manifest.70ab7b6ff0e1245ff3de3b4ca2afc846.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7670bf4815cf67dc36cc1198baf4af2d",
+    "url": "/zmx-land-house/index.html"
+  },
+  {
+    "revision": "a27d4f5022b0c4cb809b",
+    "url": "/zmx-land-house/static/css/2.56560575.chunk.css"
+  },
+  {
+    "revision": "2a94627e9153d259e3d5",
+    "url": "/zmx-land-house/static/css/main.9fcd4808.chunk.css"
+  },
+  {
+    "revision": "a27d4f5022b0c4cb809b",
+    "url": "/zmx-land-house/static/js/2.f1f77826.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/zmx-land-house/static/js/2.f1f77826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a94627e9153d259e3d5",
+    "url": "/zmx-land-house/static/js/main.182fcef2.chunk.js"
+  },
+  {
+    "revision": "a790d5f45a4589b1602f",
+    "url": "/zmx-land-house/static/js/runtime-main.7d798f55.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/zmx-land-house/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/zmx-land-house/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/zmx-land-house/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/zmx-land-house/static/media/iconfont.f58b610e.woff"
+  }
+]);
